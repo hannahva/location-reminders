17160869f3be4d321ac8c4034c684fc9837a58fd
hannahva <hannah.vanadrichem@gmail.com>
2017-11-10 19:16:52 -0800

    Initial commit

---
+++ www/js/bundle.js
@@ -0,0 +1,49 @@
+(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
+var API_KEY="AIzaSyCmAz1ooHqzQ9ChSDi33LVdtkzeLDc5MNI";
+
+
+},{}],2:[function(require,module,exports){
+const ENV = require("../../env.js");
+const apiKey = ENV.API_KEY;
+
+const mapScript = document.createElement('script');
+mapScript.setAttribute('src',`https://maps.googleapis.com/maps/api/js?key=${apiKey}&callback=initMap`);
+mapScript.async = true;
+document.body.appendChild(mapScript);
+
+var map, infoWindow;
+function initMap() {
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: -34.397, lng: 150.644},
+    zoom: 6
+  });
+  infoWindow = new google.maps.InfoWindow;
+
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function(position) {
+      var pos = {
+        lat: position.coords.latitude,
+        lng: position.coords.longitude
+      };
+
+      infoWindow.setPosition(pos);
+      infoWindow.setContent('Location found.');
+      infoWindow.open(map);
+      map.setCenter(pos);
+    }, function() {
+      handleLocationError(true, infoWindow, map.getCenter());
+    });
+  } else {
+    handleLocationError(false, infoWindow, map.getCenter());
+  }
+}
+
+function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+  infoWindow.setPosition(pos);
+  infoWindow.setContent(browserHasGeolocation ?
+                        'Error: The Geolocation service failed.' :
+                        'Error: Your Browser doesn\'t support geolocation.');
+  infoWindow.open(map);
+}
+
+},{"../../env.js":1}]},{},[2]);
